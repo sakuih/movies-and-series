46b88d03eee48a11e578225b501d0f4f885ec737
taco <taco@taco.taco>
2024-02-09 23:17:46 +0200

    models controller

--- controllers/movies_controller.go
+++ controllers/movies_controller.go
@@ -8,8 +8,33 @@ import (
 )
 
 func getFavorites(c *gin.Context) {
+  favorites, err := models.FetchAllFavorites()
+  if err != nil {
+    c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+    return
+  }
+  c.JSON(http.StatusOK, gin.H{"message": "Fetch Succesful"})
 }
+
 func addFavorite(c *gin.Context) {
+  var input models.Favorite
+
+  if err := c.ShouldBindJSON(&input); err != nil {
+
+    c.JSON(http.StatusBadRequest, gin.H{"status": "failed", "message": err.Error(), "data": nil})
+
+    return
+   }
+   savedFavorite, err := input.Save()
+
+   if err != nil {
+    c.JSON(http.StatusBadRequest,
+      gin.H{"status": "failed", "message": err.Error(), "data": nil})
+    return
+   }
+
+   c.JSON(http.StatusCreated, gin.H{"status": "success", "message": "favorite saved successfully", "data": savedFavorite})
+
 }
 func removeFavorite(c *gin.Context){
 }
--- models/favorite.go
+++ models/favorite.go
@@ -1,3 +1,47 @@
 package models
 
+import (
+  "gorm.io/gorm"
+)
+
+type Favorite struct {
+  gorm.Model
+  movieID       int
+  movieTitle    string
+  User          User
+}
+
+type User struct {
+  gorm.Model
+  userID        int
+  username      string
+  password      string
+}
+
+func (favorite *Favorite) Save() (*Favorite, error) {
+  err := Database.Model(&favorite).Create(&favorite).Error
+  if err != nil {
+    return &Favorite{}, err
+  }
+  return favorite, nil
+}
+
+func FetchAllFavorite() (*[]Favorite, error) {
+  var favorites []Favorite
+  err := Database.Find(&favorites).Error
+  if err != nil {
+    return &[]Favorite{}, err
+  }
+  return &favorites, nil
+}
+
+func DeleteStartup(id string= error {
+ err := Database.Model(&Favorite{}).Where("id = ?", id).Delete(&Favorite{}).Error
+  if err != nil {
+    return err
+   }
+  return nil
+}
+
+
 
